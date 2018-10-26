128427411219ba849878a62e361ff4256723da77
Bili Sun <bili.a.sun@gmail.com>
2018-10-25 23:36:01 -0400

    V0 fully connected crf data loader

---
+++ model_seq/fully_connected_crf_dataset.py
@@ -0,0 +1,106 @@
+from dataset import SeqDataset
+
+
+def get_char_padded_len(batch_chars, c_con, word_limit):
+    """
+    Given a limit on the number of words per sequence, find the padding length for this batch
+
+    batch_chars: batch of character arrays - words are sequence of characters, ending with c_con
+    c_con: delimiter between words
+    word_limit: max number of words per sequence
+    """
+    char_padded_len = 0
+    for chars in batch_chars:
+        assert chars[-1] == c_con
+
+        pos = 0
+        num_words = 0
+        while pos < len(chars) and num_words < word_limit:
+            if chars[pos] == c_con:
+                num_words += 1
+            pos += 1
+        char_padded_len = max(char_padded_len, pos)
+    return char_padded_len
+
+
+class FullyConnectedCRFDataset(SeqDataset):
+    """
+    Dataset for fully connected CRF
+
+    All batches have the same sequence length
+    Shorter sequences are padded to the length, longer sequences take the prefix
+
+    seq_len: fixed length of sequences per batch
+    """
+    def __init__(
+            self, dataset: list, w_pad: int, c_con: int, c_pad: int, y_start: int, y_pad: int,
+            y_size: int, batch_size: int, seq_len: int
+    ):
+        SeqDataset.__init__(self, dataset, w_pad, c_con, c_pad, y_start, y_pad, y_size, batch_size)
+
+        self.seq_len = seq_len
+
+    def batchify(self, batch, device):
+        """
+        batchify a batch of data and move to a device.
+
+        Parameters
+        ----------
+        batch: ``list``, required.
+            a sample from the encoded dataset (outputs of preprocess scripts).  
+        device: ``torch.device``, required.
+            the target device for the dataset loader.
+
+        Returns
+        ----------
+        (forward character ids,
+         forward character indices,
+         backward character ids,
+         backward character indices,
+         word ids,
+         labels)
+        """
+
+        cur_batch_size = len(batch)
+
+        batch_chars = [tup[1] for tup in batch]
+        char_padded_len = get_char_padded_len(batch_chars, self.c_con, self.seq_len)
+
+        tmp_batch =  [list() for ind in range(6)]
+
+        for instance_ind in range(cur_batch_size):
+            instance = batch[instance_ind]
+
+            char_padded_len_ins = char_padded_len - len(instance[1])
+            word_padded_len_ins = self.seq_len - len(instance[0])
+
+            if char_padded_len_ins >= 0:
+                tmp_batch[0].append(instance[1] + [self.c_pad] * char_padded_len_ins)
+                tmp_batch[2].append(instance[1][::-1] + [self.c_pad] * char_padded_len_ins)
+            else:
+                tmp_batch[0].append(instance[1][:char_padded_len])
+                tmp_batch[2].append(instance[1][:char_padded_len][::-1])
+
+            if word_padded_len_ins >= 0:
+                tmp_pf = list(itertools.accumulate(instance[3] + [0] * word_padded_len_ins))
+                tmp_pb = list(itertools.accumulate(instance[3][::-1]))[::-1] + [1] * word_padded_len_ins
+            else:
+                tmp_pf = list(itertools.accumulate(instance[3][:self.seq_len]))
+                tmp_pb = list(itertools.accumulate(instance[3][:self.seq_len][::-1]))[::-1]
+
+            tmp_batch[1].append([(x - 1) * cur_batch_size + instance_ind for x in tmp_pf])
+            tmp_batch[3].append([(x - 1) * cur_batch_size + instance_ind for x in tmp_pb])
+
+            if word_padded_len_ins >= 0:
+                tmp_batch[4].append(instance[0] + [self.w_pad] * word_padded_len_ins)
+                tmp_batch[5].append(instance[2] + [self.y_pad] * word_padded_len_ins)
+            else:
+                tmp_batch[4].append(instance[0][:self.seq_len])
+                tmp_batch[5].append(instance[2][:self.seq_len])
+
+        tbt = [torch.LongTensor(v).transpose(0, 1).contiguous() for v in tmp_batch]
+
+        tbt[1] = tbt[1].view(-1)
+        tbt[3] = tbt[3].view(-1)
+
+        return [ten.to(device) for ten in tbt]
---
+++ model_seq/test_fully_connected_crf_dataset.py
@@ -0,0 +1,20 @@
+from fully_connected_crf_dataset import get_char_padded_len
+
+
+def test_get_char_padded_len():
+    c_con = 'c'
+    word_limit = 3
+
+    batch = [
+            ['a', 'b', c_con, 'd', 'e', 'f', c_con, 'g', c_con, 'a', 'c', c_con],
+            ['a', 'b', 'd', c_con, 'd', 'e', 'f', c_con, 'g', 'd', c_con],
+            ['a', 'b', 'd', 'e', 'f', 'g', c_con],
+            ['a', c_con, 'b', c_con],
+    ]
+
+    assert get_char_padded_len(batch, c_con, word_limit) == 11
+    print('Test get_char_padded_len passed!')
+
+
+if __name__ == "__main__":
+    test_get_char_padded_len()
